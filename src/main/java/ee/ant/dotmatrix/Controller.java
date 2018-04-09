b5cc6d01a7912ecaf0c31adf1d4acc1b86d1d840
vadim <vadim@ant.ee>
2018-04-09 22:19:37 +0300

    searching for better way to present matrixes

--- src/main/java/ee/ant/dotmatrix/Controller.java
+++ src/main/java/ee/ant/dotmatrix/Controller.java
@@ -23,6 +23,14 @@ public class Controller {
     @FXML
     private RadioButton rbutton88;
     @FXML
+    private RadioButton rbutton1016;
+    @FXML
+    private RadioButton rbutton1216;
+    @FXML
+    private RadioButton rbutton1416;
+    @FXML
+    private RadioButton rbutton1616;
+    @FXML
     private GridPane dotMatrix;
     @FXML
     private TextField hexString;
@@ -36,35 +44,34 @@ public class Controller {
 
 
         // Handle 5x8 radio button event
-        rbutton58.setOnAction(event -> {
-            segments = new Segment[]{new Segment(5)};
-            drawSegments();
-        });
+        rbutton58.setOnAction(event -> createEmptyMatrix(5));
 
         // Handle 6x8 radio button event
-        rbutton68.setOnAction(event -> {
-            segments = new Segment[]{new Segment(6)};
-            drawSegments();
-        });
+        rbutton68.setOnAction(event -> createEmptyMatrix(6));
 
         // Handle 7x8 radio button event
-        rbutton78.setOnAction(event -> {
-            segments = new Segment[]{new Segment(7)};
-            drawSegments();
-        });
+        rbutton78.setOnAction(event -> createEmptyMatrix(7));
 
         // Handle 8x8 radio button event
-        rbutton88.setOnAction(event -> {
-            segments = new Segment[]{new Segment(8)};
-            drawSegments();
-        });
+        rbutton88.setOnAction(event -> createEmptyMatrix(8));
+
+        // Handle 10x16 radio button event
+        rbutton1016.setOnAction(event -> createEmptyMatrix(10));
+
+        // Handle 12x16 radio button event
+        rbutton1216.setOnAction(event -> createEmptyMatrix(12));
+
+        // Handle 14x16 radio button event
+        rbutton1416.setOnAction(event -> createEmptyMatrix(14));
+
+        // Handle 16x16 radio button event
+        rbutton1616.setOnAction(event -> createEmptyMatrix(16));
 
         // Handle draw button event
         drawButton.setOnAction(event -> parseHexString(hexString.getText()));
 
         // default is 5x8 matrix
-        segments = new Segment[]{new Segment(5)};
-        drawSegments();
+        createEmptyMatrix(5);
     }
 
     /**
@@ -72,25 +79,30 @@ public class Controller {
      */
     private void drawSegments() {
         dotMatrix.getChildren().clear();
-        for (Segment segment : segments) {
-            for (int i = 0; i < segment.getColumns(); i++) {
-                for (int j = 0; j < 8; j++) {
-                    Button btn = new Button();
-                    btn.setMinSize(16.0, 16.0);
-                    btn.setMaxSize(16.0, 16.0);
-                    btn.setOnAction(((ActionEvent click) -> {
-                        ObservableList classes = btn.getStyleClass();
-                        if (classes.size() == 1) {
-                            btn.getStyleClass().add("buttonOn");
-                        } else {
-                            btn.getStyleClass().remove(1);
-                        }
-                        generateHexString();
-                    }));
-                    dotMatrix.add(btn, i, j);
-                }
+        int x = segments[0].getColumns();
+        int y = 8;
+        if (segments.length != 1) {
+            x = 2 * x;
+            y = 2 * y;
+        }
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                Button btn = new Button();
+                btn.setMinSize(16.0, 16.0);
+                btn.setMaxSize(16.0, 16.0);
+                btn.setOnAction(((ActionEvent click) -> {
+                    ObservableList classes = btn.getStyleClass();
+                    if (classes.size() == 1) {
+                        btn.getStyleClass().add("buttonOn");
+                    } else {
+                        btn.getStyleClass().remove(1);
+                    }
+                    generateHexString();
+                }));
+                dotMatrix.add(btn, i, j);
             }
         }
+        // TODO extend for more segments
     }
 
     private void parseHexString(String text) {
@@ -131,32 +143,31 @@ public class Controller {
         }
     }
 
-//    /**
-//     * Creates empty matrix of buttons
-//     * @param x size X
-//     * @param y size Y
-//     */
-//    private void createMatrix (int x, int y) {
-//        dotMatrix.getChildren().clear();
-//        for (int i=0; i<x; i++) {
-//            for (int j=0; j<y; j++) {
-//                Button btn = new Button();
-//                btn.setId(i + "," + j);
-//                btn.setMinSize(16.0, 16.0);
-//                btn.setMaxSize(16.0, 16.0);
-//                btn.setOnAction(((ActionEvent click) -> {
-//                    ObservableList classes = btn.getStyleClass();
-//                    if (classes.size() == 1) {
-//                        btn.getStyleClass().add("buttonOn");
-//                    } else {
-//                        btn.getStyleClass().remove(1);
-//                    }
-//                    generateHexString();
-//                }));
-//                dotMatrix.add(btn, i, j);
-//            }
-//        }
-//    }
+    /**
+     * Creates empty matrix of buttons and display it
+     *
+     * @param x size X
+     */
+    private void createEmptyMatrix(int x) {
+        if (x <= 8) {
+            segments = new Segment[]{new Segment(x)};
+        } else {
+            switch (x) {
+                case 10:
+                    segments = new Segment[]{new Segment(5), new Segment(5), new Segment(5), new Segment(5)};
+                    break;
+                case 12:
+                    segments = new Segment[]{new Segment(6), new Segment(6), new Segment(6), new Segment(6)};
+                    break;
+                case 14:
+                    segments = new Segment[]{new Segment(7), new Segment(7), new Segment(7), new Segment(7)};
+                    break;
+                case 16:
+                    segments = new Segment[]{new Segment(8), new Segment(8), new Segment(8), new Segment(8)};
+            }
+        }
+        drawSegments();
+    }
 
     /**
      * Create font hex string and display it
--- src/main/java/ee/ant/dotmatrix/Main.java
+++ src/main/java/ee/ant/dotmatrix/Main.java
@@ -4,15 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
         primaryStage.setTitle("Dot matrix font generator");
-        Scene main = new Scene(root, 800, 600);
+        primaryStage.getIcons().add(new Image("ant.png"));
+        Scene main = new Scene(root, 800, 500);
         main.getStylesheets().add("styles.css");
         primaryStage.setScene(main);
         primaryStage.show();
